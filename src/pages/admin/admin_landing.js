bd24d64f555486d3a40c3dc1d028d8238825d823
Keyur <keyur.pancholi9@gmail.com>
2023-04-08 15:46:52 +0530

    kai pan

--- src/pages/admin/admin_landing.js
+++ src/pages/admin/admin_landing.js
@@ -1,56 +1,60 @@
 import {
-    Box,
-    Button,
-    Container,
-    Flex,
-    Heading,
-    Icon,
-    Stack,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  import { ReactElement } from 'react';
-  import "./admin_landing.css"
-  
-  
-  
-  const Card = ({ heading, description, icon, href }) => {
-    return (
-      <Box
-        bg={"#fff"}
-        maxW={{ base: 'full', md: '275px' }}
-        w={'full'}
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        p={5}>
-        <Stack align={'start'} spacing={2}>
-          <Flex
-            w={16}
-            h={16}
-            align={'center'}
-            justify={'center'}
-            color={'white'}
-            rounded={'full'}
-            bg={useColorModeValue('gray.100', 'gray.700')}>
-            {icon}
-          </Flex>
-          <Box mt={2}>
-            <Heading size="md">{heading}</Heading>
-            <Text mt={1} fontSize={'sm'}>
-              {description}
-            </Text>
-          </Box>
-          <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
-            Learn more
-          </Button>
-        </Stack>
-      </Box>
-    );
-  };
-  
-  export default function gridListWith() {
-    return (<div className='admin-landing-main'>
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { ReactElement } from "react";
+import "./admin_landing.css";
+import { FaBoxOpen, FaCoffee, FaGift, FaSalesforce, FaTrophy } from "react-icons/fa";
+
+const Card = ({ heading, description, icon, href }) => {
+  return (
+    <Box
+        className="admin-card"
+      bg={"#DCB9A3"}
+      maxW={{ base: "full", md: "275px" }}
+      w={"full"}
+      borderWidth="1px"
+      borderRadius="8px"
+      overflow="hidden"
+      p={5}
+      color={"#563300"}
+    >
+      <Stack align={"start"} spacing={2}>
+        <Flex
+          w={16}
+          h={16}
+          align={"center"}
+          justify={"center"}
+          color={"white"}
+          rounded={"full"}
+          bg={useColorModeValue("gray.100", "gray.700")}
+        >
+          {icon}
+        </Flex>
+        <Box mt={2}>
+          <Heading size="md">{heading}</Heading>
+          <Text mt={1} fontSize={"sm"}>
+            {description}
+          </Text>
+        </Box>
+        {/* <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
+          Learn more
+        </Button> */}
+      </Stack>
+    </Box>
+  );
+};
+
+export default function gridListWith() {
+  return (
+    <div className="admin-landing-main">
       <Box p={4}>
         {/* <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
           <Heading fontSize={{ base: '2xl', sm: '4xl' }} fontWeight={'bold'}>
@@ -61,57 +65,57 @@ import {
             obcaecati ut cupiditate pariatur, dignissimos, placeat amet officiis.
           </Text>
         </Stack> */}
-  
-        <Container maxW={'5xl'} mt={12}>
+
+        <Container maxW={"5xl"} mt={12}>
           <Flex flexWrap="wrap" gridGap={6} justify="center">
             <Card
-              heading={'Scratch and Win'}
-              icon={<Icon w={10} h={10} />}
+              heading={"Scratch and Win"}
+              icon={<Icon as={FaGift} w={10} h={10} />}
               description={
-                'Create a scratch and win coupon which will be dispatched '
+                "Create a scratch and win coupon which will be dispatched "
               }
-              href={'#'}
+              href={"#"}
               className="admin-card"
             />
             <Card
-              heading={'Create Combos'}
-              icon={<Icon w={10} h={10} />}
+              heading={"Create Combos"}
+              icon={<Icon as={FaCoffee} w={10} h={10} />}
               description={
-                'Create combos for users that increase sales and customize products based on sales'
+                "Create combos for users that increase sales and customize products based on sales"
               }
-              href={'#'}
+              href={"#"}
               className="admin-card"
             />
             <Card
-              heading={'View Sales'}
-              icon={<Icon w={10} h={10} />}
+              heading={"View Sales"}
+              icon={<Icon as={FaSalesforce} w={10} h={10} />}
               description={
-                'Detailed statistics about current sales used for analysis and planning strategy'
+                "Detailed statistics about current sales used for analysis and planning strategy"
               }
-              href={'#'}
+              href={"#"}
               className="admin-card"
             />
             <Card
-              heading={'Loyalty program'}
-              icon={<Icon w={10} h={10} />}
+              heading={"Loyalty program"}
+              icon={<Icon as={FaTrophy} w={10} h={10} />}
               description={
-                'Program that awards points and maintains a leaderboard of customers'
+                "Program that awards points and maintains a leaderboard of customers"
               }
-              href={'#'}
+              href={"#"}
               className="admin-card"
             />
             <Card
-              heading={'Manage Products'}
-              icon={<Icon w={10} h={10} />}
+              heading={"Manage Products"}
+              icon={<Icon as={FaBoxOpen} w={10} h={10} />}
               description={
-                'List of all products along with inventory management like adding new products, deleting existing products.'
+                "List of all products along with inventory management like adding new products, deleting existing products."
               }
-              href={'#'}
+              href={"#"}
               className="admin-card"
             />
           </Flex>
         </Container>
       </Box>
-      </div>
-    );
-  }
+    </div>
+  );
+}
