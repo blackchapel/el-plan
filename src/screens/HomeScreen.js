7338777c862ba800d7915fdb452fc46e7bc1495c
blackchapel <kc.dark@protonmail.com>
2023-04-08 22:37:58 +0530

    home screen + Drawer Component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,101 @@
 import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+
+import Carousel from 'react-native-snap-carousel';
+import { Feather } from '@expo/vector-icons'
+
+import BannerComponent from '../components/BannerComponent';
+import avatar from './../../assets/avatar.jpg'
+import { sliderData } from '../utils/data';
+import { windowWidth } from '../utils/dimensions';
 
 const HomeScreen = () => {
+    const renderBanner = ({ item, index }) => {
+        return <BannerComponent image={item} />;
+    };
+    
     return (
-        <SafeAreaView>
-            <View>
-                <Text>Hello kc!</Text>
+        <SafeAreaView style={styles.body}>
+            <View style={styles.headerView}>
+                <Text style={styles.helloText}>Hello kc!</Text>
+
+                <Image source={avatar} style={styles.image} />
             </View>
+
+            <View style={styles.search}>
+                <Feather
+                    name="search"
+                    size={20}
+                    color={'#6D756E'}
+                    style={{ marginRight: 5, paddingTop: 5, }}
+                />
+                <TextInput placeholder="search" />
+            </View>
+
+            <View style={styles.placeholder}>
+                <Text style={styles.placeholderText}>Our Products</Text>
+                <TouchableOpacity>
+                    <Text style={{ color: '#6D756E' }}>See all</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Carousel
+                ref={(c) => {
+                    this._carousel = c;
+                }}
+                data={sliderData}
+                renderItem={renderBanner}
+                sliderWidth={windowWidth - 30}
+                itemWidth={300}
+                loop={true}
+            />
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        backgroundColor: '#FEEDDC',
+        paddingHorizontal: 20,
+        paddingTop: 40,
+        width: '100%'
+    },
+    headerView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    helloText: {
+        fontWeight: 'bold',
+        fontSize: 22,
+        color: '#563300'
+    },
+    image: {
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: '#563300'
+    },
+    search: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: '#6D756E',
+        marginVertical: 10,
+        padding: 10
+    },
+    placeholder: {
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    placeholderText: {
+      color: '#563300',
+      fontSize: 15,
+      fontWeight: 'bold'
+    }
+})
 
 export default HomeScreen;
---
+++ src/utils/data.js
@@ -0,0 +1,14 @@
+export const sliderData = [
+    {
+      title: 'First Game',
+      image: require('../../assets/game-1.jpg'),
+    },
+    {
+      title: 'Second Game',
+      image: require('../../assets/game-2.jpg'),
+    },
+    // {
+    //   title: 'Third Game',
+    //   image: require('../assets/game-3.png'),
+    // },
+];
