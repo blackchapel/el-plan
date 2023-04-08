4c3a1a717fabd061192359509277c7667c150244
Keyur <keyur.pancholi9@gmail.com>
2023-04-09 00:29:45 +0530

    Improved frontend

--- src/pages/admin/manage_products.js
+++ src/pages/admin/manage_products.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, ReactNode } from "react";
 import axios from "axios";
+import "./manage_products.css";
+import coffee from "../../assets/coffee.webp";
 import {
   Tabs,
   TabList,
@@ -14,17 +16,61 @@ import {
   Input,
   Button,
   Center,
-  useColorModeValue,
   Text,
   Stack,
   Image,
+  SimpleGrid,
+  Spacer,
+  Select,
 } from "@chakra-ui/react";
 import AdminNavbar from "../../components/admin/admin_navbar/adminNavbar";
 
 const ManageProducts = () => {
-  const IMAGE =
-    "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
   const [products, setProducts] = useState([]);
+  const [file, setFile] = React.useState(null);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState();
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.set("name", name);
+    formData.set("category", category);
+    formData.set("thumbnail", file);
+    formData.set("price", price);
+    formData.set("description", description);
+
+    const data = {
+      name: formData.get("name"),
+      category: formData.get("category"),
+      thumbnail: file,
+      price: formData.get("price"),
+      description: formData.get("description"),
+    };
+    console.log(data);
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.post(
+        "https://el-plan-production.up.railway.app/api/product",
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      alert(response.data.message);
+      console.log(response);
+    } catch (err) {
+      alert(err.response.data.message);
+      console.log(err);
+    }
+  };
+
   const getProduct = async () => {
     const token = localStorage.getItem("token");
     try {
@@ -33,13 +79,46 @@ const ManageProducts = () => {
         {
           headers: {
             Authorization: `Bearer ${token}`,
-          }
+          },
         }
       );
       setProducts(response.data.data.products);
+      console.log(products);
     } catch (error) {
       alert(error.response.data.message);
     }
+  };
+
+  const productDeleteHandler = async (id) => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.delete(
+        "https://el-plan-production.up.railway.app/api/product/" + id,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      alert(response.data.message);
+      const new_prods = products.filter((prod) => prod._id !== id);
+      setProducts(new_prods);
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
   };
 
   useEffect(() => {
@@ -49,119 +128,188 @@ const ManageProducts = () => {
   return (
     <>
       <AdminNavbar />
+
       <Tabs>
         <TabList>
           <Tab>View Products</Tab>
           <Tab>Create product</Tab>
         </TabList>
-
-        <TabPanels>
+        <TabPanels backgroundColor={"#FEEDDC"}>
           <TabPanel className="manage-tabs">
-            <p>one!</p>
-            {products && products.map((prod, index) => {
-              return(
-              <Center py={12} key={index}>
-                <Box
-                  role={"group"}
-                  p={6}
-                  maxW={"330px"}
-                  w={"full"}
-                //   bg={useColorModeValue("white", "gray.800")}
-                  bg = "white"
-                  _dark={{bg: "gray.800"}}
-                  boxShadow={"2xl"}
-                  rounded={"lg"}
-                  pos={"relative"}
-                  zIndex={1}
-                >
-                  <Box
-                    rounded={"lg"}
-                    mt={-12}
-                    pos={"relative"}
-                    height={"230px"}
-                    _after={{
-                      transition: "all .3s ease",
-                      content: '""',
-                      w: "full",
-                      h: "full",
-                      pos: "absolute",
-                      top: 5,
-                      left: 0,
-                      backgroundImage: `url(${IMAGE})`,
-                      filter: "blur(15px)",
-                      zIndex: -1,
-                    }}
-                    _groupHover={{
-                      _after: {
-                        filter: "blur(20px)",
-                      },
-                    }}
-                  >
-                    <Image
-                      rounded={"lg"}
-                      height={230}
-                      width={282}
-                      objectFit={"cover"}
-                      src={IMAGE}
-                    />
-                  </Box>
-                  <Stack pt={10} align={"center"}>
-                    <Text
-                      color={"gray.500"}
-                      fontSize={"sm"}
-                      textTransform={"uppercase"}
-                    >
-                      Brand
-                    </Text>
-                    <Heading
-                      fontSize={"2xl"}
-                      fontFamily={"body"}
-                      fontWeight={500}
-                    >
-                      Nice Chair, pink
-                    </Heading>
-                    <Stack direction={"row"} align={"center"}>
-                      <Text fontWeight={800} fontSize={"xl"}>
-                        Rs. {prod.price}
-                      </Text>
-                    </Stack>
-                  </Stack>
-                </Box>
-              </Center>);
-            })}
-            
+            <SimpleGrid columns={3} spacing={3}>
+              {products &&
+                products.map((prod, index) => {
+                  return (
+                    <Center py={12} key={index}>
+                      <Box
+                        role={"group"}
+                        p={6}
+                        maxW={"330px"}
+                        w={"full"}
+                        //   bg={useColorModeValue("white", "gray.800")}
+                        bg="white"
+                        _dark={{ bg: "gray.800" }}
+                        boxShadow={"2xl"}
+                        rounded={"lg"}
+                        pos={"relative"}
+                        zIndex={1}
+                      >
+                        <Box
+                          rounded={"lg"}
+                          mt={-12}
+                          pos={"relative"}
+                          height={"230px"}
+                          _after={{
+                            transition: "all .3s ease",
+                            content: '""',
+                            w: "full",
+                            h: "full",
+                            pos: "absolute",
+                            top: 5,
+                            left: 0,
+                            backgroundImage: `url(${prod.thumbnail})`,
+                            filter: "blur(15px)",
+                            zIndex: -1,
+                          }}
+                          _groupHover={{
+                            _after: {
+                              filter: "blur(20px)",
+                            },
+                          }}
+                        >
+                          <Image
+                            rounded={"lg"}
+                            height={230}
+                            width={282}
+                            objectFit={"cover"}
+                            src={prod.thumbnail}
+                          />
+                        </Box>
+                        <Stack pt={10} align={"center"} position={"relative"}>
+                          <Heading
+                            fontSize={"2xl"}
+                            fontFamily={"body"}
+                            fontWeight={500}
+                          >
+                            {prod.name}
+                          </Heading>
+                          <Text
+                            color={"gray.500"}
+                            fontSize={"sm"}
+                            textTransform={"uppercase"}
+                            height={100}
+                          >
+                            {prod.description}
+                          </Text>
+                          <Flex
+                            flexDirection={"row"}
+                            justifyContent={"space-around"}
+                            width="100%"
+                          >
+                            <Stack direction={"row"} align={"center"}>
+                              <Text fontWeight={800} fontSize={"xl"}>
+                                Rs. {prod.price}
+                              </Text>
+                            </Stack>
+                            <Spacer />
+                            <Button
+                              color={"red"}
+                              onClick={() => {
+                                productDeleteHandler(prod._id);
+                              }}
+                            >
+                              Delete
+                            </Button>
+                          </Flex>
+                        </Stack>
+                      </Box>
+                    </Center>
+                  );
+                })}
+            </SimpleGrid>
           </TabPanel>
           <TabPanel className="manage-tabs">
-            <Flex width="full" align="center" justifyContent="center">
-              <Box p={2}>
-                <Box textAlign="center">
-                  <Heading>Add product details</Heading>
-                </Box>
-                <Box my={4} textAlign="left">
-                  <form>
-                    <FormControl>
-                      <FormLabel>Name</FormLabel>
-                      <Input type="text" placeholder="" />
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel>Category</FormLabel>
-                      <Input type="text" placeholder="" />
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel>Price</FormLabel>
-                      <Input type="number" placeholder="" />
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel>Description</FormLabel>
-                      <Input type="text" placeholder="" />
-                    </FormControl>
-                    <Button mt={4} type="submit">
-                      Submit
-                    </Button>
-                  </form>
-                </Box>
+            <SimpleGrid columns={2} spacing={1}>
+              <Box>
+                <Center>
+                  <img src={coffee} className="rotate" />
+                </Center>
               </Box>
-            </Flex>
+              <Flex width="full" align="center" justifyContent="center">
+                <Box p={2}>
+                  <Box textAlign="center">
+                    <Heading>Add product details</Heading>
+                  </Box>
+                  <Box my={4} textAlign="left">
+                    <form>
+                      <FormControl>
+                        <FormLabel>Name</FormLabel>
+                        <Input
+                          type="text"
+                          placeholder=""
+                          value={name}
+                          onChange={handleNameChange}
+                          border={"solid"}
+                        />
+                      </FormControl>
+                      <FormControl>
+                        <FormLabel>Category</FormLabel>
+                        {/* <Input
+                        type="text"
+                        placeholder=""
+                        value={category}
+                        onChange={handleCategoryChange}
+                      /> */}
+                        <Select
+                          placeholder="Select category"
+                          value={category}
+                          onChange={handleCategoryChange}
+                          border={"solid"}
+                        >
+                          <option value="hotCoffee">Hot Coffee</option>
+                          <option value="coldCoffee">Cold Coffee</option>
+                          <option value="coffeeCoolers">Coffee Coolers</option>
+                          <option value="notCoffee">Not coffee</option>
+                          <option value="manualBrew">Manual Brew</option>
+                        </Select>
+                      </FormControl>
+
+                      <FormControl>
+                        <FormLabel>File</FormLabel>
+                        <Input
+                          type="file"
+                          onChange={(e) => setFile(e.target.files[0])}
+                          border={"none"}
+                        ></Input>
+                      </FormControl>
+                      <FormControl>
+                        <FormLabel>Price</FormLabel>
+                        <Input
+                          type="number"
+                          placeholder=""
+                          value={price}
+                          onChange={handlePriceChange}
+                          border={"solid"}
+                        />
+                      </FormControl>
+                      <FormControl>
+                        <FormLabel>Description</FormLabel>
+                        <Input
+                          type="text"
+                          placeholder=""
+                          value={description}
+                          onChange={handleDescriptionChange}
+                          border={"solid"}
+                        />
+                      </FormControl>
+                      <Button mt={4} type="submit" onClick={handleSubmit}>
+                        Submit
+                      </Button>
+                    </form>
+                  </Box>
+                </Box>
+              </Flex>
+            </SimpleGrid>
           </TabPanel>
         </TabPanels>
       </Tabs>
